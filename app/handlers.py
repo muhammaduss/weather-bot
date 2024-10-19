0e176835dd8054618be0b262c8da6a1d1dca1b07
muhammaduss <muhammad20040130@gmail.com>
2024-10-19 18:53:46 +0300

    adding api retrieval

--- app/handlers.py
+++ app/handlers.py
@@ -32,7 +32,36 @@ async def get_city(message: Message, state: FSMContext):
 
     query = API_URL + "&appid=" + API_TOKEN + "&q=" + data["city"]
     print(query)
-    response = requests.get(query, timeout=10)
+    try:
+        response = requests.get(query, timeout=10)
+        if response.status_code == 200:
+            await message.answer(
+                f"Погода для города {data['city']}:\n\nТемпература: "
+                + f"{round(response.json()['main']['temp'] - 273.15)} градусов"
+                + " Цельсия"
+                + f"\nВлажность: {response.json()['main']['humidity']}%\n"
+                + f"Описание: {response.json()['weather'][0]['description']}",
+                reply_markup=kb.main,
+            )
+        else:
+            await message.answer(
+                "Введенный город, вероятно, не существует\n"
+                + "Убедитесь, что вы правильно вводите название города",
+                reply_markup=kb.main,
+            )
 
-    await message.answer(f"Yout city is: {response.json()}")
-    await state.clear()
+    except requests.exceptions.Timeout:
+        await message.answer(
+            "Cервис временно недоступен, попробуйте попозже",
+            reply_markup=kb.main,
+        )
+
+        await state.clear()
+
+
+@router.message()
+async def another_message(message: Message):
+    await message.answer(
+        "Пожалуйста, нажмите на кнопку 'Узнать погоду' ниже и введите город",
+        reply_markup=kb.main,
+    )
